5cb3584dbf2534c0bf70b3131ac335d50569ed1a
Pratik Sharma <sharma.pratik2016@gmail.com>
2020-05-15 15:37:30 +0530

    Updates

---
+++ precache-manifest.8fb146dedf220cd8181ef0508653bcad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267514742450a97fedb7065030dfd4e1",
+    "url": "/CovidandMentalHealth/index.html"
+  },
+  {
+    "revision": "589549e6e62d061ab154",
+    "url": "/CovidandMentalHealth/static/css/main.beb94f52.chunk.css"
+  },
+  {
+    "revision": "2b3821d195cf7ce70c42",
+    "url": "/CovidandMentalHealth/static/js/2.ae7a65af.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CovidandMentalHealth/static/js/2.ae7a65af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589549e6e62d061ab154",
+    "url": "/CovidandMentalHealth/static/js/main.a3c2cc15.chunk.js"
+  },
+  {
+    "revision": "5b0345a9a3134f73e283",
+    "url": "/CovidandMentalHealth/static/js/runtime-main.f6399800.js"
+  }
+]);
